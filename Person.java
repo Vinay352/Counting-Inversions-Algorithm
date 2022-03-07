a5cd213087596b00e5e10f15aa1140dcdd1f442b
Vinay352 <vinay.j3097@gmail.com>
2022-03-06 21:31:57 -0500

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,29 @@
+/*
+ * Person.java
+ *
+ * Version:
+ *     $2$
+ */
+
+/**
+ * CSCI-665
+ *
+ *
+ *   @author: Omkar Morogiri,om5692
+ *   @author: Vinay Jain,vj9898
+ *
+ *
+ */
+
+// class to store information about age and height of people
+public class Person{
+
+    int age;
+    float height;
+
+    Person( int age, float height ){
+        this.age = age;
+        this.height = height;
+    }
+
+}
